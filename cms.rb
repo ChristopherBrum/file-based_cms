95b098d199b64be67a1487e6713d25b2d5e1d14c
Christopher Brum <christopher.brum@gmail.com>
2022-03-11 14:01:45 -0800

    added test suite and tests

--- cms.rb
+++ cms.rb
@@ -2,19 +2,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-root = File.expand_path("..", __FILE__)
+configure do
+  enable :sessions
+  set :session_secret, 'secret_session_id'
+
+  # use Rack::Session::Cookie, :key => 'rack.session',
+  #                            :path => '/',
+  #                            :secret => 'secret session id'
+end
+
+ROOT = File.expand_path("..", __FILE__) + "/data/"
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
-  
-  erb :index
+  @files = Dir.glob("#{ROOT}*")
+              .map { |path| File.basename(path) }
+
+  erb :index, layout: :layout
 end
 
 get "/:filename" do
-  path = "#{root}/data/#{params[:filename]}"
+  path = ROOT + params[:filename]
 
-  headers["Content-Type"] = "text/plain"
-  File.read(path)
+  if File.exist?(path)
+    session[:success] = "File found"
+
+    headers["Content-Type"] = "text/plain"
+    File.read(path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -2,6 +2,8 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 require_relative "../cms.rb"
 
@@ -29,5 +31,36 @@ class CMSTest < Minitest::Test
     assert_equal(200, last_response.status)
     assert_equal("text/plain", last_response["Content-Type"])
     assert_includes(last_response.body, "Maecenas mollis tincidunt euismod.")
+  end
+  
+  def test_filename
+    get "/changes.txt"
+    
+    assert_equal(200, last_response.status)
+    assert_equal("text/plain", last_response["Content-Type"])
+    assert_includes(last_response.body, "Nam augue quam")
+  end
+
+  def test_filename
+    get "/history.txt"
+    
+    assert_equal(200, last_response.status)
+    assert_equal("text/plain", last_response["Content-Type"])
+    assert_includes(last_response.body, "2017 - Ruby 2.5 released.")
+    assert_includes(last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby.")
+  end
+
+  def test_document_not_found
+    get "/something.txt"
+
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "something.txt does not exist.")
+
+    get "/"
+    refute_includes(last_response.body, "something.txt does not exist.")
   end
 end
