29920d67131ecf02a55f1bf9be38aad46f6a58bc
Christopher Brum <christopher.brum@gmail.com>
2022-03-11 18:46:25 -0800

    more cms features

--- cms.rb
+++ cms.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
 
 configure do
   enable :sessions
@@ -13,6 +14,23 @@ end
 
 ROOT = File.expand_path("..", __FILE__) + "/data/"
 
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    File.read(path)
+  when ".md"
+    render_markdown(content)
+  end
+end
+
+# index page lists all files
 get "/" do
   @files = Dir.glob("#{ROOT}*")
               .map { |path| File.basename(path) }
@@ -20,16 +38,33 @@ get "/" do
   erb :index, layout: :layout
 end
 
+# displays selected file
 get "/:filename" do
-  path = ROOT + params[:filename]
+  file_path = ROOT + params[:filename]
+  @file_name = params[:filename]
 
-  if File.exist?(path)
-    session[:success] = "File found"
-
-    headers["Content-Type"] = "text/plain"
-    File.read(path)
+  if File.exist?(file_path) 
+    load_file_content(file_path)
   else
-    session[:message] = "#{params[:filename]} does not exist."
+    session[:message] = "#{@file_name} does not exist."
     redirect "/"
   end
+end
+
+# display the edit page for a file
+get "/:filename/edit" do 
+  file_path = ROOT + params[:filename]
+  @file_name = params[:filename]
+  @file_contents = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = ROOT + params[:filename]
+  @file_name = params[:filename]
+
+  File.write(file_path, params[:content])
+  session[:message] = "#{@file_name} has been updated."
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -20,7 +20,7 @@ class CMSTest < Minitest::Test
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
     assert_includes(last_response.body, "changes.txt")
-    assert_includes(last_response.body, "about.txt")
+    assert_includes(last_response.body, "about.md")
     assert_includes(last_response.body, "history.txt")
 
   end
@@ -62,5 +62,27 @@ class CMSTest < Minitest::Test
 
     get "/"
     refute_includes(last_response.body, "something.txt does not exist.")
+  end
+
+  def test_markdown_file
+    get "/about.md"
+
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "<h1>Ruby is...</h1>")
+  end
+
+  def test_editing_document
+    get "/history.txt/edit"
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "<textarea")
+    assert_includes(last_response.body, "1993 - Yukihiro Matsumoto")
+  end
+
+  def test_updating_document
+    post "/history.txt", content: "new content"
+
+    # assert_equal(302, last_response.status)
   end
 end
