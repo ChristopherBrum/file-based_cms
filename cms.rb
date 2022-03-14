5afecd7fd4dcd492fdfe7613dbd4bfa36d294a01
Christopher Brum <christopher.brum@gmail.com>
2022-03-14 15:37:54 -0700

    added more cms features

--- cms.rb
+++ cms.rb
@@ -6,13 +6,21 @@ require 'redcarpet'
 configure do
   enable :sessions
   set :session_secret, 'secret_session_id'
-
+  set :views         , File,expand_path()
   # use Rack::Session::Cookie, :key => 'rack.session',
   #                            :path => '/',
   #                            :secret => 'secret session id'
 end
 
-ROOT = File.expand_path("..", __FILE__) + "/data/"
+# ROOT = File.expand_path("..", __FILE__) + "/data/"
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data/", __FILE__)
+  else
+    File.expand_path("../data/", __FILE__)
+  end
+end
 
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
@@ -24,23 +32,46 @@ def load_file_content(path)
   case File.extname(path)
   when ".txt"
     headers["Content-Type"] = "text/plain"
-    File.read(path)
+    content
   when ".md"
-    render_markdown(content)
+    erb render_markdown(content)
   end
 end
 
 # index page lists all files
 get "/" do
-  @files = Dir.glob("#{ROOT}*")
-              .map { |path| File.basename(path) }
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map { |path| File.basename(path) }
 
-  erb :index, layout: :layout
+  erb :index
+end
+
+# Display form for adding a new file
+get "/new" do
+  erb :new
+end
+
+# creates a new file
+post "/create" do
+  filename = params[:filename].to_s
+
+  if filename.size == 0
+    session[:message] = "A name is required"
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+
+    session[:message] = "#{params[:filename]} was created."
+    redirect "/"
+  end
 end
 
 # displays selected file
 get "/:filename" do
-  file_path = ROOT + params[:filename]
+  file_path = File.join(data_path, params[:filename])
   @file_name = params[:filename]
 
   if File.exist?(file_path) 
@@ -53,18 +84,32 @@ end
 
 # display the edit page for a file
 get "/:filename/edit" do 
-  file_path = ROOT + params[:filename]
+  file_path = File.join(data_path, params[:filename])
+
   @file_name = params[:filename]
   @file_contents = File.read(file_path)
 
   erb :edit
 end
 
+# Update the selected file
 post "/:filename" do
-  file_path = ROOT + params[:filename]
-  @file_name = params[:filename]
+  file_name = params[:filename]
+  file_path = File.join(data_path, file_name)
 
   File.write(file_path, params[:content])
-  session[:message] = "#{@file_name} has been updated."
+
+  session[:message] = "#{file_name} has been updated."
+  redirect "/"
+end
+
+# Deletes a file
+post "/:filename/delete" do
+  file_name = params[:filename]
+  file_path = File.join(data_path, file_name)
+
+  File.delete(file_name)
+
+  session[:message] = "#{file_name} has been deleted."
   redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -4,6 +4,7 @@ require "minitest/autorun"
 require "rack/test"
 require "minitest/reporters"
 Minitest::Reporters.use!
+require "fileutils"
 
 require_relative "../cms.rb"
 
@@ -14,40 +15,52 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content="")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document("about.md")
+    create_document("changes.txt")
+    create_document("history.txt")
+
     get "/"
 
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "about.md")
+    assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "history.txt")
-
   end
 
-  def test_filename
-    get "/about.txt"
+  def test__markdown_file
+    create_document("about.md", "open source programming language")
+
+    get "/about.md"
     
     assert_equal(200, last_response.status)
-    assert_equal("text/plain", last_response["Content-Type"])
-    assert_includes(last_response.body, "Maecenas mollis tincidunt euismod.")
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, " programming ")
   end
   
-  def test_filename
+  def test_text_file
+    create_document("changes.txt", "Nam augue quam, feugiat id suscipit quis, vestibulum vitae magna.")
+
     get "/changes.txt"
     
     assert_equal(200, last_response.status)
     assert_equal("text/plain", last_response["Content-Type"])
     assert_includes(last_response.body, "Nam augue quam")
-  end
-
-  def test_filename
-    get "/history.txt"
-    
-    assert_equal(200, last_response.status)
-    assert_equal("text/plain", last_response["Content-Type"])
-    assert_includes(last_response.body, "2017 - Ruby 2.5 released.")
-    assert_includes(last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby.")
   end
 
   def test_document_not_found
@@ -64,15 +77,8 @@ class CMSTest < Minitest::Test
     refute_includes(last_response.body, "something.txt does not exist.")
   end
 
-  def test_markdown_file
-    get "/about.md"
-
-    assert_equal(200, last_response.status)
-    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, "<h1>Ruby is...</h1>")
-  end
-
   def test_editing_document
+    create_document("history.txt", "1993 - Yukihiro Matsumoto dreams up Ruby.")
     get "/history.txt/edit"
 
     assert_equal(200, last_response.status)
@@ -81,8 +87,54 @@ class CMSTest < Minitest::Test
   end
 
   def test_updating_document
-    post "/history.txt", content: "new content"
+    create_document("changes.txt", "Nam augue quam, feugiat id suscipit quis, ")
 
-    # assert_equal(302, last_response.status)
+    get "/changes.txt" 
+    assert_includes(last_response.body, "Nam augue quam,")
+
+    post "/changes.txt", content: "No longer contains changes"
+
+    assert_equal(302, last_response.status)
+    refute_includes(last_response.body, "Nam augue quam,")
+  end
+
+  def test_view_new_document_form
+    get "/new"
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "new document")
+    assert_includes(last_response.body, "Create")
+
+  end
+
+  def test_create_new_document
+    post "/create", filename: "test.txt"
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+    assert_includes(last_response.body, "test.txt was created")
+
+    get "/"
+    assert_includes(last_response.body, "test.txt")
+  end
+
+  def test_create_new_document_without_filename
+    post "/create", filename: ""
+    assert_equal(422, last_response.status)
+    assert_includes(last_response.body, "A name is required")
+  end
+
+  def test_delete_document
+    create_document("deletable.md", "I hope this works!")
+
+    post "/:filename/destroy"
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+    assert_includes(last_response.body, "deletable.md has been deleted")
+
+    get "/" 
+    assert_equal(200, last_response.status)
+    refute_includes(last_response.body, "deletable.md")
   end
 end
